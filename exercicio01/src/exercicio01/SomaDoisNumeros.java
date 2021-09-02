660e6be778d5c865df6d1de15668dc9616a082ae
vfsouza <vini.f.souza@hotmail.com>
2021-09-02 17:06:55 -0300

    Comentários adicionados

--- exercicio01/src/exercicio01/SomaDoisNumeros.java
+++ exercicio01/src/exercicio01/SomaDoisNumeros.java
@@ -2,19 +2,23 @@ package exercicio01;
 import java.util.*;
 
 class SomaDoisNumeros {
+	// Objeto Scanner para entrada de dados
 	public static Scanner sc = new Scanner(System.in);
 	
 	public static void main(String[] args) {
 		int num1, num2, soma;
 		
+		// Entrada do primeiro número da soma
 		System.out.println("Digite um numero");
 		num1 = sc.nextInt();
 		
+		// Entrada do segundo número da soma
 		System.out.println("Digite outro numero");
 		num2 = sc.nextInt();
 		
 		soma = num1 + num2;
 		
+		// Saída da soma dos dois números
 		System.out.println("Soma: " + soma);
 	}
 }
